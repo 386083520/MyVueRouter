194b1fb2181631044d3746dee1cde6cda06797e6
386083520 <386083520@qq.com>
2021-08-17 18:28:08 +0800

    项目构建

---
+++ dist/vue-router.js
@@ -0,0 +1,13 @@
+/*!
+  * vue-router v1.0.0
+  * (c) 2021 Evan You
+  * @license MIT
+  */
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+}((function () { 'use strict';
+
+	console.log('gsd666');
+
+})));
